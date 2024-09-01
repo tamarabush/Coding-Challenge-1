b8368ac9ce0e0354792d14a8894817f520fb1586
tamarabush <tamara.abush14@outlook.com>
2024-09-01 17:39:21 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Lana Del Rey";
+const employeeID = 90210;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
