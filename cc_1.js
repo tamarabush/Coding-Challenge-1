a0949b66b9ee59f5c3dd04b4f3efcbfb39a70270
tamarabush <tamara.abush14@outlook.com>
2024-09-01 17:59:52 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,17 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+
+
+// Task 2: Compund Data Types
+
+let products =["Candle", "Lightbulb", "Plants"] 
+
+const productDetails = {
+        price: 10.00,
+        name: products,
+        inStock: 150,
+}
+
+console.log(products, productDetails);
